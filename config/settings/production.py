3d14fd873f8a0d0724222108662e583901a53a15
vijenderpanda <64571936+vijenderpanda@users.noreply.github.com>
2023-06-14 15:10:59 +0530

    ch

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["trial-miraki.miraki.ai"])
+ALLOWED_HOSTS = "*"
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -131,7 +131,7 @@ ANYMAIL = {}
 
 LOGGING = {
     "version": 1,
-    "disable_existing_loggers": True,
+    "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s",
