df7a120a03227be9228c776094b6ca66eed62865
vijenderpanda <64571936+vijenderpanda@users.noreply.github.com>
2023-06-21 17:25:44 +0530

    ch

--- config/settings/production.py
+++ config/settings/production.py
@@ -45,9 +45,9 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-# SESSION_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-# CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
@@ -197,6 +197,7 @@ LOGGING = {
 # django-rest-framework
 # -------------------------------------------------------------------------------
 # Tools that generate code samples can use SERVERS to point to the correct domain
+print("SUBDOMAIN", env("SUBDOMAIN"))
 SPECTACULAR_SETTINGS["SERVERS"] = [  # noqa: F405
     {"url": f'http://org.miraki.ai', "description": "Org Demo server"},
 ]
