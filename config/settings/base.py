c113c1f3a08d09b48a8ac74951f4cbfd0aef804e
vijenderpanda <64571936+vijenderpanda@users.noreply.github.com>
2023-08-17 19:29:46 +0530

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -353,10 +353,10 @@ SOCIALACCOUNT_FORMS = {"signup": "miraki.users.forms.UserSocialSignupForm"}
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
